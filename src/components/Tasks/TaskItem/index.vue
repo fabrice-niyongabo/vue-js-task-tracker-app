1469b81290bbae4b7a3c9d3c80dcf1772a7ce279
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-09-07 00:55:35 +0200

    styling a component

--- src/components/Tasks/TaskItem/index.vue
+++ src/components/Tasks/TaskItem/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="task-container border">
-    <h3>{{ task.title }}</h3>
+    <div>
+      <h3>{{ task.title }}</h3>
+      <p class="m-0">{{ task.date }} | {{ task.time }}</p>
+    </div>
+    <i class="fas fa-times"></i>
   </div>
 </template>
 <script>
@@ -10,9 +14,16 @@ export default {
 </script>
 <style lang="css" scoped>
 .task-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
   margin: 1rem 0px;
   text-align: left;
   padding: 1rem;
   border-radius: 10px;
+}
+.fa-times:hover {
+  color: red;
+  cursor: pointer;
 }
 </style>
